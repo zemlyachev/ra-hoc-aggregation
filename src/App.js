7d038d17c2788b9bc899aa38cdd7684382fb1f6d
Andrey Zemlyachev <kvintilian@gmail.com>
2022-09-01 14:00:08 +0300

    Update App.js
    Create aggregation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,41 @@ import React from "react";
 import YearTable from "./components/YearTable";
 import SortTable from "./components/SortTable";
 import MonthTable from "./components/MonthTable";
-export default class App extends React.Component {
-  state = {
-    list: [],
-    isLoading: false,
+
+function withAggregateList(WrappedComponent, propNameParam) {
+  return function (props) {
+    const resultList =
+      propNameParam === "sort"
+        ? [...props.list].sort((a, b) => {
+            return a.date.split("-").join("") - b.date.split("-").join("");
+          })
+        : [...props.list].map((item) => {
+            let newProp = "";
+            if (propNameParam === "year") {
+              newProp = item.date.split("-")[0];
+            } else if (propNameParam === "month") {
+              newProp = item.date.split("-")[1];
+            }
+            return { [propNameParam]: newProp, amount: item.amount };
+          });
+
+    return <WrappedComponent list={resultList} />;
   };
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: [],
+      isLoading: false,
+    };
+
+    this.WithAggregatedMonthTable = withAggregateList(MonthTable, "month");
+    this.WithAggregatedYearTable = withAggregateList(YearTable, "year");
+    this.WithAggregatedSortTable = withAggregateList(SortTable, "sort");
+  }
 
   componentDidMount() {
     this.setState({ isLoading: true });
@@ -22,12 +52,13 @@ export default class App extends React.Component {
 
   render() {
     const { list, isLoading } = this.state;
+
     return (
       <div id="app">
         {isLoading && <progress />}
-        <MonthTable list={list} />
-        <YearTable list={list} />
-        <SortTable list={list} />
+        <this.WithAggregatedMonthTable list={list} />
+        <this.WithAggregatedYearTable list={list} />
+        <this.WithAggregatedSortTable list={list} />
       </div>
     );
   }
