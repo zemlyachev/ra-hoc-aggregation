f10a033573f94703232e2566c66acbcaa04763a8
Andrey Zemlyachev <kvintilian@gmail.com>
2022-09-01 12:20:43 +0300

    Update App.js
    Load data

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import React from "react";
 import YearTable from "./components/YearTable";
 import SortTable from "./components/SortTable";
 import MonthTable from "./components/MonthTable";
-
-// TODO:
-// 1. Загрузите данные с помощью fetch: https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hoc/aggregation/data/data.json
-// 2. Не забудьте вынести URL в переменные окружения (не хардкодьте их здесь)
-// 3. Положите их в state
 export default class App extends React.Component {
   state = {
     list: [],
+    isLoading: false,
   };
 
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    fetch(process.env.REACT_APP_DATA_URL)
+      .then((response) => response.json())
+      .then((result) =>
+        this.setState({
+          list: result.list,
+          isLoading: false,
+        })
+      );
+  }
+
   render() {
-    const { list } = this.state;
+    const { list, isLoading } = this.state;
     return (
       <div id="app">
+        {isLoading && <progress />}
         <MonthTable list={list} />
         <YearTable list={list} />
         <SortTable list={list} />
